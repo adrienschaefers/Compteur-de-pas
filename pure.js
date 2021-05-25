58e9c83d5e393a14e1aca64bf5fe9bc26dbb5d81
Adrien Schaefers <adrien.schaefers@hetic.net>
2021-05-26 00:06:35 +0200

    Ajout de la fonction Math.random pour manipuler de manière alétaoire le BG color des éléments

--- pure.js
+++ pure.js
@@ -3,14 +3,22 @@
 //changer le texte qui dit "Hello" --> "Hello[NOM]""
 
 //pick the h1 tag
-const headerTag = document.querySelector("h1")
+// const headerTag = document.querySelector("h1")
 
 //then change the html content
-headerTag.innerHTML = "Hello mon gars"
+// headerTag.innerHTML = "Hello mon gars"
 
 //then change the html tag to a background of red
-headerTag.style.backgroundColor = "var(--primary-orange"
+// headerTag.style.backgroundColor = "var--primary-orange"
 
 //font-size goes big 
-headerTag.style.fontSize = ("64px")
+// headerTag.style.fontSize = ("64px")
 
+//let's puck ALL of the H1 of the page
+const headerTags = document.querySelectorAll("h1")
+
+//then for each tag let it ahve a BG color
+headerTags.forEach(h1 => {
+    const hue = 360 * Math.random() //Choisit une valeur de 0 à 360 (aléatoire)
+    h1.style.backgroundColor = "hsl(" + hue + ", 100%, 50%)" //Ici on utilise la valeur HSL --> figma
+})
