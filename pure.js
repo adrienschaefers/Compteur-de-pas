7aeb3a520d10e165c708f926806d53e962397c33
Adrien Schaefers <adrien.schaefers@hetic.net>
2021-05-25 23:49:00 +0200

    ajout de svg (rectangle + cercle) + JS

---
+++ pure.js
@@ -0,0 +1,16 @@
+//alert("Hello ma men")
+
+//changer le texte qui dit "Hello" --> "Hello[NOM]""
+
+//pick the h1 tag
+const headerTag = document.querySelector("h1")
+
+//then change the html content
+headerTag.innerHTML = "Hello mon gars"
+
+//then change the html tag to a background of red
+headerTag.style.backgroundColor = "var(--primary-orange"
+
+//font-size goes big 
+headerTag.style.fontSize = ("64px")
+
