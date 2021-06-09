a2ac1844906a90ab2673f47ae627887527e40544
Adrien Schaefers <adrien.schaefers@hetic.net>
2021-06-09 21:57:43 +0200

    Ajout de la grille en utilisant le modulo et la f math.floor()

--- month.js
+++ month.js
@@ -1,10 +1,23 @@
 const monthSvg = d3.select("svg.month")
 
+
+// 
+const radiusScale = d3.scaleSqrt()
+    .domain([0, 30000])
+    .range([0, 50])
+
+// On crée une autre variable pour paramétrer les nuances de couleurs
+const colorScale = d3.scaleSqrt()
+    .domain([10000, 30000])
+    .range(["blue","red"])
+
 monthSvg
     .selectAll("circle")
     .data(monthData)
     .enter()
     .append("circle")
-    .attr("cx", (d, i) => {return i * 28 + 10})
-    .attr("cy", (d, i) => {return  40})
-    .attr("r", (d, i) => {return 5})
+    // Ici on utilise modulo et math.floor pour créer une grille .
+    .attr("cx", (d, i) => { return (i % 7) * 120 + 60})
+    .attr("cy", (d, i) => { return  Math.floor(i / 7) * 100 + 60})
+    .attr("r", (d, i) => { return radiusScale(d) })
+    .attr("fill", (d, i) => { return colorScale(d) })
